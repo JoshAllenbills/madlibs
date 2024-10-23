5ba853d4643760a7ac377414c70d6cd323c8dc34
Colin <colind2824@tuckahoeschools.org>
2024-10-23 12:29:31 +0000

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+Adjective1=input("type an adjective: ")
+Noun1=input("type a noun: ")
+Adjective2=input("type a second adjective: ")
+Noun2=input("type a second noun: ")
+Adjective3=input("type a third adjective: ")
+Noun3=input("type a third noun: ")
+Adjective4=input("type a fourth adjective: ")
+Noun4=input("type a fourth noun: ")
+Adjective5=input("type a fifth adjective: ")
+Noun5=input("type a fifth noun: ")
+Adjective6=input("type a sixth adjective: ")
+Noun6=input("type a sixth noun: " )
+Adjective7=input("type a seventh adjective: ")
+Noun7=input("type a seventh noun: ")
+Adjective8=input("type a eight adjective: ")
+Noun8=input("type a eight noun: ")
+Adjective9=input("type a ninth adjective: ")
+Noun9=input("type a ninth noun: ")
+Adjective10=input("type a tenth adjective: ")
+Noun10=input("type a tenth noun: ")
+print(f"""Once upon a time there was a {Adjective1} {Noun1}
+{Adjective2} {Noun2} the little girls who beleived that the {Adjective3} was 
+{Noun3}{Adjective4} {Noun4}{Adjective5} {Noun5} {Adjective6} {Noun6} {Adjective7} {Noun7} {Adjective8} {Noun8} {Adjective9} {Noun9} 
+{Adjective10}{Noun10}
+""")
